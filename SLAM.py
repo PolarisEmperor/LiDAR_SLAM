35ba30b5c686f4749c9281ad3b966cd6b8518751
Dhruva Chakravarthi <70412345+PolarisEmperor@users.noreply.github.com>
2023-02-02 23:47:14 -0500

    added more test cases and modified key-point identification algorithm

--- SLAM.py
+++ SLAM.py
@@ -8,26 +8,26 @@ import math
 DataPointsPerScan = DataPointsPerScanAdjust = 512
 # minimum distance between other points to be determined outlier
 MinimumOutlierDistance = 3
-# minimum change in angle (in degrees) for critical point to be recorded
-MinimumAngleChange = 60
+# minimum change in angle (in degrees) for key point to be recorded
+MinimumAngleChange = 50
 
 # setup stuff
 fig = plt.figure()
 np.set_printoptions(suppress=True)
 
 # reading distance data
-LiDAR_raw_data_file = open("data2.txt", "r")
+LiDAR_raw_data_file = open("room1/room1data5.txt", "r")
 point_distances = np.empty(DataPointsPerScan)
 angles = np.arange(0, 2*np.pi, (2*np.pi / DataPointsPerScan), dtype=float)
 outliers = []
+key_points = []
 for i in range(DataPointsPerScan):
-    point_distances[i] = float(LiDAR_raw_data_file.readline())
+    point_distances[i] = float(LiDAR_raw_data_file.readline().strip())
 
 # determine and remove outliers
 for i in range(DataPointsPerScan):
     if point_distances[i] == np.inf:
         outliers.append(i)
-
 for i in range(DataPointsPerScan):
     prevPointFar = False
     nextPointFar = False
@@ -75,21 +75,14 @@ for i in range(DataPointsPerScanAdjust):
     point_distance_1 = point_distances[i]
     point_distance_2 = point_distances[i - 1]
     point_distance_3 = math.sqrt(abs(math.pow(point_data_x[i] - point_data_x[i - 2], 2) + math.pow(point_data_y[i] - point_data_y[i - 2], 2)))
-    point_angles[i] = np.arccos((math.pow(point_distance_1, 2) + math.pow(point_distance_2, 2) - math.pow(point_distance_3, 2))/(2 * point_distance_1 * point_distance_2))
-    # adjust angles if not interior angle
 
-    if point_data_y[i - 1] > 0:
-        if point_data_y[i - 1] < point_data_y[i - 2] and point_data_y[i - 1] < point_data_y[i]:
-            point_angles[i] = 2 * np.pi - point_angles[i]
+    # floating point error, out of arccos domain
+    if ((math.pow(point_distance_1, 2) + math.pow(point_distance_2, 2) - math.pow(point_distance_3, 2))/(2 * point_distance_1 * point_distance_2)) >= 1:
+        point_angles[i] = np.arccos(1)
+    elif ((math.pow(point_distance_1, 2) + math.pow(point_distance_2, 2) - math.pow(point_distance_3, 2))/(2 * point_distance_1 * point_distance_2)) <= -1:
+        point_angles[i] = np.arccos(-1)
     else:
-        if point_data_y[i - 1] > point_data_y[i - 2] and point_data_y[i - 1] > point_data_y[i]:
-            point_angles[i] = 2 * np.pi - point_angles[i]
-    if point_data_x[i - 1] > 0:
-        if point_data_x[i - 1] < point_data_x[i - 2] and point_data_x[i - 1] < point_data_x[i]:
-            point_angles[i] = 2 * np.pi - point_angles[i]
-    else:
-        if point_data_x[i - 1] > point_data_x[i - 2] and point_data_x[i - 1] > point_data_x[i]:
-            point_angles[i] = 2 * np.pi - point_angles[i]
+        point_angles[i] = np.arccos((math.pow(point_distance_1, 2) + math.pow(point_distance_2, 2) - math.pow(point_distance_3, 2))/(2 * point_distance_1 * point_distance_2))
 
 # calculate rate of change between angles
 roc_slope_angles = np.empty(DataPointsPerScanAdjust, dtype=float)
@@ -101,21 +94,22 @@ roc_point_distances = np.empty(DataPointsPerScanAdjust, dtype=float)
 for i in range(DataPointsPerScanAdjust):
     roc_point_distances[i] = abs(point_distances[i - 1] - point_distances[i])
 
-# mark critical points
+# record key points
 for i in range(DataPointsPerScanAdjust):
-    if abs(roc_slope_angles[i]) > np.deg2rad(MinimumAngleChange):
-        colors[i - 1] = [1, 0, 0]
-        sizes[i - 1] = 50
-    if roc_point_distances[i] > MinimumOutlierDistance:
-        colors[i - 1] = colors[i] = [1, 0, 0]
-        sizes[i - 1] = sizes[i] = 50
+    if (abs(roc_slope_angles[i]) > np.deg2rad(MinimumAngleChange)) and (not ((i - 2) in outliers)):
+        key_points.append(i - 2)
+    if (roc_point_distances[i] > MinimumOutlierDistance) and (not ((i - 1) in outliers)):
+        key_points.append(i - 1)
+
+for i in key_points:
+    colors[i] = [1, 0, 0]
+    sizes[i] = 50
 
 # show plot
-plt.scatter(0, 0, color=[0, 0, 1], s=300)
+plt.scatter(0, 0, color=[0, 0, 0], s=500)
+plt.scatter(0, 0, color=[0, 1, 1], s=300)
 plt.scatter(point_data_x, point_data_y, c=colors, s=sizes)
 plt.show()
 
 # cleanup
 LiDAR_raw_data_file.close()
-
-
