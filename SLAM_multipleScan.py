cc2991c52ba42ba202ae007b75b5c83fe4f67dd8
Dhruva Chakravarthi <70412345+PolarisEmperor@users.noreply.github.com>
2023-03-05 14:04:49 -0500

    added new dataset

--- SLAM_multipleScan.py
+++ SLAM_multipleScan.py
@@ -2,14 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-
-# SLAM options
-def adjust(value, limit):
-    if value >= 512:
-        return value - limit
-    return value
-
-
 # data points collected per 360 rotation
 DataPointsPerScan = DataPointsPerScanAdjust = 512
 # minimum distance between other points to be determined outlier
@@ -22,13 +14,13 @@ ClusterThreshold = 20
 # setup stuff
 fig = plt.figure()
 np.set_printoptions(suppress=True)
-LiDAR_raw_data_file = open("multiplescansdata2", "r")
+LiDAR_raw_data_file = open("/Users/dhruvachakravarthi/Downloads/erebus-master 3/game/controllers/my_controller/b.txt", "r")
 
 # reading distance data
 angles = np.arange(0, 2 * np.pi, (2 * np.pi / DataPointsPerScan), dtype=float)
 coords_x = []
 coords_y = []
-for i in range(7):
+for i in range(13):
     point_distances = np.empty(DataPointsPerScan)
     LiDAR_x, LiDAR_y, LiDAR_yaw = LiDAR_raw_data_file.readline().strip().split(' ')
 
@@ -37,16 +29,14 @@ for i in range(7):
         coords_x.append(float(LiDAR_x) + (point_distances[j] * np.cos(-float(LiDAR_yaw) + angles[j])))
         coords_y.append(float(LiDAR_y) + (point_distances[j] * np.sin(-float(LiDAR_yaw) + angles[j])))
 
-    # LiDAR_raw_data_file.readline()
-
     # show plot
     plt.scatter(float(LiDAR_x), float(LiDAR_y), color=[0, 0, 0], s=500)
     plt.scatter(float(LiDAR_x), float(LiDAR_y), color=[1, 0, 1], s=300)
 
+    LiDAR_raw_data_file.readline()
+
 plt.scatter(coords_x, coords_y, color=[0, 0, 0])
 plt.gca().invert_yaxis()
-
-
 plt.show()
 
 # cleanup
